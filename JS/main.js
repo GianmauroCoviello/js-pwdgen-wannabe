25008b9dd765db62b00ffd1badf374e30e9d4a5b
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-05-16 17:04:53 +0200

    [DOCS] Piccola modifica.

--- JS/main.js
+++ JS/main.js
@@ -7,7 +7,7 @@ let surname = prompt('Iserisci il tuo cognome');
 // VARIABILE COLORE 
 let preferite_color = prompt('Iserisci il tuo colore preferito');
 
-// VARIABILE ANNO DI NASCITA  
+// VARIABILE NUMERO 21  
 let number = '21';
 
 let password = name + surname + preferite_color + number
